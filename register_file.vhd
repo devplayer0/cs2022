c13724b4b842cdebaabf5357f5263c948940564e
Jack O'Sullivan <jackos1998@gmail.com>
2019-02-26 11:22:23 +0000

    Update register file for datapath

--- register_file.vhd
+++ register_file.vhd
@@ -3,19 +3,14 @@ use ieee.std_logic_1164.all;
 
 entity register_file is
 	port(
-		reg_src:	in	std_logic_vector(2 downto 0);
 		dst_reg:	in	std_logic_vector(2 downto 0);
 		clock :		in	std_logic;
-		data_src:	in std_logic;
 		data :		in	std_logic_vector(15 downto 0);
-		r0 :		out	std_logic_vector(15 downto 0);
-		r1 :		out	std_logic_vector(15 downto 0);
-		r2 :		out	std_logic_vector(15 downto 0);
-		r3 :		out	std_logic_vector(15 downto 0);
-		r4 :		out	std_logic_vector(15 downto 0);
-		r5 :		out	std_logic_vector(15 downto 0);
-		r6 :		out	std_logic_vector(15 downto 0);
-		r7 :		out	std_logic_vector(15 downto 0)
+		load :		in	std_logic;
+		a_select :	in	std_logic_vector(2 downto 0) := "000";
+		b_select :	in	std_logic_vector(2 downto 0) := "000";
+		a : 		out	std_logic_vector(15 downto 0);
+		b : 		out	std_logic_vector(15 downto 0)
 	);
 end register_file;
 
@@ -42,15 +37,6 @@ architecture behavior of register_file is
 		);
 	end component;
 
-	component mux2_16bit
-		port(
-			s :		in	std_logic;
-			ln0 :	in	std_logic_vector(15 downto 0);
-			ln1 :	in	std_logic_vector(15 downto 0);
-			z :		out	std_logic_vector(15 downto 0)
-		);
-	end component;
-
 	component reg16
 		port(
 			load :	in	std_logic;
@@ -61,72 +47,74 @@ architecture behavior of register_file is
 	end component;
 
 	signal dec_load_reg : std_logic_vector(0 to 7);
-	signal load_data, reg_src_data : std_logic_vector(15 downto 0);
+	signal l_r0, l_r1, l_r2, l_r3, l_r4, l_r5, l_r6, l_r7 : std_logic;
 	signal r0_q, r1_q, r2_q, r3_q, r4_q, r5_q, r6_q, r7_q : std_logic_vector(15 downto 0);
 begin
-	reg0: reg16 port map (
-		load => dec_load_reg(0),
-		clock => clock,
-		data => load_data,
-		q => r0_q
-	);
-	reg1: reg16 port map (
-		load => dec_load_reg(1),
-		clock => clock,
-		data => load_data,
-		q => r1_q
-	);
-	reg2: reg16 port map (
-		load => dec_load_reg(2),
-		clock => clock,
-		data => load_data,
-		q => r2_q
-	);
-	reg3: reg16 port map (
-		load => dec_load_reg(3),
-		clock => clock,
-		data => load_data,
-		q => r3_q
-	);
-	reg4: reg16 port map (
-		load => dec_load_reg(4),
-		clock => clock,
-		data => load_data,
-		q => r4_q
-	);
-	reg5: reg16 port map (
-		load => dec_load_reg(5),
-		clock => clock,
-		data => load_data,
-		q => r5_q
-	);
-	reg6: reg16 port map (
-		load => dec_load_reg(6),
-		clock => clock,
-		data => load_data,
-		q => r6_q
-	);
-	reg7: reg16 port map (
-		load => dec_load_reg(7),
-		clock => clock,
-		data => load_data,
-		q => r7_q
-	);
-
 	dreg_decoder: decoder_3to8 port map (
 		a => dst_reg,
 		q => dec_load_reg
 	);
 
-	dsrc_mux: mux2_16bit port map (
-		s => data_src,
-		ln0 => data,
-		ln1 => reg_src_data,
-		z => load_data
+	l_r0 <= load and dec_load_reg(0);
+	l_r1 <= load and dec_load_reg(1);
+	l_r2 <= load and dec_load_reg(2);
+	l_r3 <= load and dec_load_reg(3);
+	l_r4 <= load and dec_load_reg(4);
+	l_r5 <= load and dec_load_reg(5);
+	l_r6 <= load and dec_load_reg(6);
+	l_r7 <= load and dec_load_reg(7);
+
+	reg0: reg16 port map (
+		load => l_r0,
+		clock => clock,
+		data => data,
+		q => r0_q
+	);
+	reg1: reg16 port map (
+		load => l_r1,
+		clock => clock,
+		data => data,
+		q => r1_q
+	);
+	reg2: reg16 port map (
+		load => l_r2,
+		clock => clock,
+		data => data,
+		q => r2_q
+	);
+	reg3: reg16 port map (
+		load => l_r3,
+		clock => clock,
+		data => data,
+		q => r3_q
+	);
+	reg4: reg16 port map (
+		load => l_r4,
+		clock => clock,
+		data => data,
+		q => r4_q
+	);
+	reg5: reg16 port map (
+		load => l_r5,
+		clock => clock,
+		data => data,
+		q => r5_q
+	);
+	reg6: reg16 port map (
+		load => l_r6,
+		clock => clock,
+		data => data,
+		q => r6_q
+	);
+	reg7: reg16 port map (
+		load => l_r7,
+		clock => clock,
+		data => data,
+		q => r7_q
 	);
 
-	reg_src_mux: mux8_16bit port map (
-		s => reg_src,
+	ra_mux: mux8_16bit port map (
+		s => a_select,
 		ln0 => r0_q,
 		ln1 => r1_q,
 		ln2 => r2_q,
@@ -135,15 +123,18 @@ begin
 		ln5 => r5_q,
 		ln6 => r6_q,
 		ln7 => r7_q,
-		z => reg_src_data
+		z => a
 	);
-
-	r0 <= r0_q;
-	r1 <= r1_q;
-	r2 <= r2_q;
-	r3 <= r3_q;
-	r4 <= r4_q;
-	r5 <= r5_q;
-	r6 <= r6_q;
-	r7 <= r7_q;
+	rb_mux: mux8_16bit port map (
+		s => b_select,
+		ln0 => r0_q,
+		ln1 => r1_q,
+		ln2 => r2_q,
+		ln3 => r3_q,
+		ln4 => r4_q,
+		ln5 => r5_q,
+		ln6 => r6_q,
+		ln7 => r7_q,
+		z => b
+	);
 end;
--- register_file_tb.vhd
+++ register_file_tb.vhd
@@ -7,50 +7,35 @@ end register_file_tb;
 architecture behavior of register_file_tb is
 	component register_file
 	port(
-		reg_src:	in	std_logic_vector(2 downto 0);
 		dst_reg:	in	std_logic_vector(2 downto 0);
 		clock :		in	std_logic;
-		data_src:	in std_logic;
 		data :		in	std_logic_vector(15 downto 0);
-		r0 :		out	std_logic_vector(15 downto 0);
-		r1 :		out	std_logic_vector(15 downto 0);
-		r2 :		out	std_logic_vector(15 downto 0);
-		r3 :		out	std_logic_vector(15 downto 0);
-		r4 :		out	std_logic_vector(15 downto 0);
-		r5 :		out	std_logic_vector(15 downto 0);
-		r6 :		out	std_logic_vector(15 downto 0);
-		r7 :		out	std_logic_vector(15 downto 0)
+		load :		in	std_logic;
+		a_select :	in	std_logic_vector(2 downto 0);
+		b_select :	in	std_logic_vector(2 downto 0);
+		a : 		out	std_logic_vector(15 downto 0);
+		b : 		out	std_logic_vector(15 downto 0)
 	);
 	end component;
 
-	signal reg_src:		std_logic_vector(2 downto 0) := "000";
 	signal dst_reg:		std_logic_vector(2 downto 0);
 	signal clock :		std_logic := '0';
-	signal data_src:	std_logic;
 	signal data :		std_logic_vector(15 downto 0);
-	signal r0 :			std_logic_vector(15 downto 0);
-	signal r1 :			std_logic_vector(15 downto 0);
-	signal r2 :			std_logic_vector(15 downto 0);
-	signal r3 :			std_logic_vector(15 downto 0);
-	signal r4 :			std_logic_vector(15 downto 0);
-	signal r5 :			std_logic_vector(15 downto 0);
-	signal r6 :			std_logic_vector(15 downto 0);
-	signal r7 :			std_logic_vector(15 downto 0);
+	signal load :		std_logic;
+	signal a_select:	std_logic_vector(2 downto 0);
+	signal b_select:	std_logic_vector(2 downto 0);
+	signal a :			std_logic_vector(15 downto 0);
+	signal b :			std_logic_vector(15 downto 0);
 begin
 	uut: register_file port map (
-		reg_src => reg_src,
 		dst_reg => dst_reg,
 		clock => clock,
-		data_src => data_src,
 		data => data,
-		r0 => r0,
-		r1 => r1,
-		r2 => r2,
-		r3 => r3,
-		r4 => r4,
-		r5 => r5,
-		r6 => r6,
-		r7 => r7
+		load => load,
+		a_select => a_select,
+		b_select => b_select,
+		a => a,
+		b => b
 	);
 
 	clock_proc: process
@@ -60,15 +45,22 @@ begin
 	end process;
 	sim_proc: process
 	begin
-		data_src <= '0';
 		dst_reg <= "010";
 		data <= x"dead";
+		load <= '1';
+		a_select <= "010";
 		wait for 20 ns;
 
 		data <= x"0000";
-		data_src <= '1';
 		dst_reg <= "110";
-		reg_src <= "010";
+		load <= '0';
+		a_select <= "110";
+		wait for 20 ns;
+
+		dst_reg <= "111";
+		data <= x"beef";
+		load <= '1';
+		b_select <= "111";
 		wait for 20 ns;
 
 		wait for 20 ns;
